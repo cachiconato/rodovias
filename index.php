75e7d551b64f796f32051742963b83c176595a2f
chiconato <cachiconato@gmail.com>
2013-09-08 04:56:38 -0300

    Adding highlight with KML coordinates

--- index.php
+++ index.php
@@ -41,23 +41,31 @@ html, body {
     <script>
 function initialize() {
 
+  var brazil = new google.maps.LatLng(-18.771115, -42.758789);
+
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(10, -140),
-    zoom: 3,
+    center: brazil,
+    zoom: 5,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
   var layer = new google.maps.FusionTablesLayer({
     query: {
-      select: 'latitude,longitude',
-      from: '1OgvgVtgVRIHG6luR0e2f-SjLv9C0QLv6c7VDM8s'
+      select: 'col1',
+      from: '1lgEWgtJ9YE8ftmoxZf-eiD8t6a0mwFZOSynU5lU',
+      where: "col0 contains ignoring case 'BR-'"
     },
-    heatmap: {
-      enabled: true
-    }
+    styleId: 2,
+    templateId: 2,
+    ui_token: 'AIGcsfNKJ-n4J5_o5f5nZSUjB18uxsG1oQ',
+    heatmap: {enabled: false}
   });
 
   layer.setMap(map);
+
+  google.maps.event.addListener(layer, 'click', function () {
+      console.log('layer click');
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
