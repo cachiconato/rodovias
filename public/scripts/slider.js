29e06497c2f12dcba88fe4b7c6222a68707518af
fwitzke <pigroxalot@gmail.com>
2013-10-23 12:05:32 -0200

    formatting slider date

--- public/scripts/slider.js
+++ public/scripts/slider.js
@@ -9,6 +9,11 @@ $(function(){
       min: new Date(2007, 0, 1),
       max: new Date(2013, 5, 30)
     },
+    formatter: function(val) {
+      var month = val.getMonth() + 1;
+      var year  = val.getFullYear();
+      return month + '-' + year;
+    },
     step: {
       months: 1
     }
