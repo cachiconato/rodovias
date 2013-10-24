374100e4857c8928dc739934359f6728078cab8a
Glauber Ramos <glauberamos@gmail.com>
2013-10-24 19:24:39 -0200

    fixing layout on popup

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -350,10 +350,8 @@ function showPopUp(clickEvent, rows) {
 
   var html = [];
   html.push('  <h2>' + popupData.name + '</h2>');
-  html.push('  <img class="icon" src="images/caraccident.png" />');
-  html.push('  <span class="number">' + popupData.accidents + '</span>');
-  html.push('  <img class="icon" src="images/dead.png" />');
-  html.push('  <span class="number">' + popupData.deaths + '</span><br>');
+  html.push('  <span class="column"><img class="icon" src="images/caraccident.png" /><span class="number">' + popupData.accidents + '</span></span>');
+  html.push('  <span class="column"><img class="icon" src="images/dead.png" /><span class="number">' + popupData.deaths + '</span><br></span>');
   _.each(top5, function(t){
     html.push('  <span class="causa"><span class="percentage">' + t.percentage + '%</span> ' + t.cause);
     html.push('  <span class="progress-bar"><span class="progress-color" style="width: ' + t.percentage + '%"' + '></span></span>');
